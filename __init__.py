76a06c90049f9be1752a0617f4e9384d874df4f9
Douglas Gómez <doueduar2018@gmail.com>
2025-03-11 22:00:28 -0600

    add proyect logic

--- __init__.py
+++ __init__.py
@@ -1,42 +1,11 @@
+
 import tkinter as tk
 from views import VistaLibros
-from views import VistaUsuarios
-
+from Proyect_Final.views import VistaUsuarios
 import libros
-libros.create_table()
-
-# Pruebas de funcionalidades
-print(libros.agregar_libro('code1','anonimo','code1'))
-print(libros.agregar_libro('code2','anonimo','code2'))
-print(libros.agregar_libro('code3','anonimo','code3'))
-print(libros.ver_libros())
-print(libros.eliminar_libro('code3'))
-print(libros.buscar_libro_titulo('code2'))
-print(libros.buscar_libro_isbm('code2'))
-print(libros.buscar_libro_autor('anonimo'))
-
 import usuarios
-usuarios.create_table()
 
-# Pruebas de Funcionalidades
-print(usuarios.registrar_usuario('user1','Dou',22))
-print(usuarios.registrar_usuario('user2','Eduar',12))
-print(usuarios.registrar_usuario('user3','Eduar',12))
-print(usuarios.ver_usuarios())
-print(usuarios.eliminar_usuario('user3'))
-
-import prestamos
-prestamos.createTable()
-print(prestamos.prestar_libro('code1','user1'))
-print(prestamos.devolver_libro('code1','user1'))
-print(prestamos.prestar_libro('code2','user2'))
-print(prestamos.devolver_libro('code2','user2'))
-
-import morosidad
-morosidad.create_table()
-
-
-import utilidades
+#import utilidades
 
 root = tk.Tk()
 
