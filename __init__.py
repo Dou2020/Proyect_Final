cb931099d5e95259d9dd6b38803d83206e946c50
Rudy Reyes <rudyreyes202031213@cunoc.edu.gt>
2025-03-11 18:35:20 -0600

    agregando disenio libros y usuarios

--- __init__.py
+++ __init__.py
@@ -6,31 +6,31 @@ import libros
 libros.create_table()
 
 # Pruebas de funcionalidades
-print(libros.agregar_libro('code1','anonimo','code1'))
-print(libros.agregar_libro('code2','anonimo','code2'))
-print(libros.agregar_libro('code3','anonimo','code3'))
-print(libros.ver_libros())
-print(libros.eliminar_libro('code3'))
-print(libros.buscar_libro_titulo('code2'))
-print(libros.buscar_libro_isbm('code2'))
-print(libros.buscar_libro_autor('anonimo'))
+#print(libros.agregar_libro('code1','anonimo','code1'))
+#print(libros.agregar_libro('code2','anonimo','code2'))
+#print(libros.agregar_libro('code3','anonimo','code3'))
+#print(libros.ver_libros())
+#print(libros.eliminar_libro('code3'))
+#print(libros.buscar_libro_titulo('code2'))
+#print(libros.buscar_libro_isbm('code2'))
+#print(libros.buscar_libro_autor('anonimo'))
 
 import usuarios
 usuarios.create_table()
 
 # Pruebas de Funcionalidades
-print(usuarios.registrar_usuario('user1','Dou',22))
-print(usuarios.registrar_usuario('user2','Eduar',12))
-print(usuarios.registrar_usuario('user3','Eduar',12))
-print(usuarios.ver_usuarios())
-print(usuarios.eliminar_usuario('user3'))
+#print(usuarios.registrar_usuario('user1','Dou',22))
+#print(usuarios.registrar_usuario('user2','Eduar',12))
+#print(usuarios.registrar_usuario('user3','Eduar',12))
+#print(usuarios.ver_usuarios())
+#print(usuarios.eliminar_usuario('user3'))
 
 import prestamos
 prestamos.createTable()
-print(prestamos.prestar_libro('code1','user1'))
-print(prestamos.devolver_libro('code1','user1'))
-print(prestamos.prestar_libro('code2','user2'))
-print(prestamos.devolver_libro('code2','user2'))
+#print(prestamos.prestar_libro('code1','user1'))
+#print(prestamos.devolver_libro('code1','user1'))
+#print(prestamos.prestar_libro('code2','user2'))
+#print(prestamos.devolver_libro('code2','user2'))
 
 import morosidad
 morosidad.create_table()
